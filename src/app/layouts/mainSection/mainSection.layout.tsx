255dc47ea13a62a91d0d570e7c0672db11de645d
Aredu89 <aredu_r89@hotmail.com>
2023-08-10 16:45:43 -0600

    HLD-Starting with main section

--- src/app/layouts/mainSection/mainSection.layout.tsx
+++ src/app/layouts/mainSection/mainSection.layout.tsx
@@ -4,7 +4,13 @@ import styles from './mainSection.module.css';
 
 const MainSection = () => (
   <section className={styles.sectionContainer}>
-    Main Section
+    <h4>Find the top software engineers in Latin America</h4>
+    <h2>
+      <p>Save money on development costs:</p>
+      <p>Hiring staff from LATAM can save you up to 40% in salaries.</p>
+    </h2>
+    <h5>Make your hiring process smoother with our help.</h5>
+    <div className={styles.animation}>Animation!</div>
   </section>
 );
 
