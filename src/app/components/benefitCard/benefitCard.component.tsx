f4bde3ef49eb8d9c49ca974567bdf9a0a12fa416
Aredu89 <aredu_r89@hotmail.com>
2023-08-29 11:25:48 -0600

    HLD-animation for cards

--- src/app/components/benefitCard/benefitCard.component.tsx
+++ src/app/components/benefitCard/benefitCard.component.tsx
@@ -1,13 +1,34 @@
-import React from 'react';
+'use client'
+
+import React, { useEffect } from 'react';
 
 import { benefitCardProps } from './benefitCard.types';
 import styles from './benefitCard.module.css';
 
-const BenefitCard = ({title, description, rightAlignment}: benefitCardProps) => (
-  <div className={styles.benefitCardContainer}>
-    <h3 className={styles.title}>{title}</h3>
-    {description.map((text, indx) => (<p key={indx} className={styles.description}>{text}</p>))}
-  </div>
-);
+const BenefitCard = ({title, description, rightAlignment}: benefitCardProps) => {
+  useEffect(() => {
+    const observer = new IntersectionObserver(entries => {
+      entries.forEach(entry => {
+        // If the element is visible
+        if (entry.isIntersecting) {
+          // Add the animation class
+          entry.target.classList.add(styles.animation);
+        }
+      });
+    });
+  
+    const cardElements = document.getElementsByClassName(styles.benefitCardContainer);
+    Array.from(cardElements).forEach(el => {
+      observer.observe(el);
+    });
+  }, [])
+
+  return (
+    <div className={`${styles.benefitCardContainer} ${rightAlignment ? styles.right : styles.left}`}>
+      <h3 className={styles.title}>{title}</h3>
+      {description.map((text, indx) => (<p key={indx} className={styles.description}>{text}</p>))}
+    </div>
+  );
+};
 
 export default BenefitCard;
