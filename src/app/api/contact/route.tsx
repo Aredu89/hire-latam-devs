df0ab87772008531f0a9f7eec60e5d62b6368c59
Aredu89 <aredu_r89@hotmail.com>
2024-01-17 11:26:08 -0600

    HLD - api route to send email from contact form

---
+++ src/app/api/contact/route.tsx
@@ -0,0 +1,58 @@
+import { NextResponse, NextRequest } from 'next/server';
+const nodemailer = require('nodemailer');
+
+export async function POST(request: NextRequest) {
+  const username = process.env.NEXT_PUBLIC_EMAIL_USERNAME;
+  const password = process.env.NEXT_PUBLIC_EMAIL_PASSWORD;
+  const myEmail = process.env.NEXT_PUBLIC_PERSONAL_EMAIL;
+
+  const reqData = await request.json();
+  const {
+    name,
+    email,
+    company,
+    message
+  } = reqData;
+
+  const transporter = nodemailer.createTransport({
+    host: "smtp-mail.outlook.com",
+    port: 587,
+    tls: {
+      ciphers: "SSLv3",
+      rejectUnauthorized: false,
+    },
+    auth: {
+      user: username,
+      pass: password
+    }
+  });
+
+  try {
+    const mail = await transporter.sendMail({
+      from: username,
+      to: myEmail,
+      replyTo: email,
+      subject: `HLD - Contact from website: ${email}`,
+      html: `
+      <p>Name: ${name} </p>
+      <p>Company: ${company} </p>
+      <p>Email: ${email} </p>
+      <p>Message: ${message} </p>
+      `,
+    });
+    return NextResponse.json({ message: "Success: email was sent" });
+  } catch (error) {
+      console.log(error)
+      return NextResponse.json({ message: "COULD NOT SEND MESSAGE" }, { status: 500 });
+  };
+};
+
+export const config = {
+  api: {
+    bodyParser: {
+      sizeLimit: '1mb',
+    },
+  },
+  // Specifies the maximum allowed duration for this function to execute (in seconds)
+  maxDuration: 5,
+};
--- src/app/components/form/components/submit/submit.component.tsx
+++ src/app/components/form/components/submit/submit.component.tsx
@@ -1,18 +1,41 @@
-import { useContext } from "react";
+'use client'
+
+import { useContext, useState } from "react";
 import { FormContext } from "../../context/form.context";
 import { Button } from '../../..';
 
 const Submit = () => {
   const { checkErrors, state } = useContext(FormContext);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
 
-  const onSubmit = () => {
-    console.log('---on submit---');
+  const onSubmit = async () => {
     const thereAreErrors = checkErrors();
     if(thereAreErrors) {
-      console.log('There are errors!')
+      console.log('There are errors in the form')
     } else {
-      console.log('No errors');
-      console.log('Form STATE::: ', state);
+      const body = JSON.stringify(state);
+      setLoading(true);
+      try {
+        const response = await fetch('/api/contact', {
+          method: 'POST',
+          body: body,
+          headers: {
+            "Content-Type": "application/json",
+          }
+        });
+        if (!response.ok) {
+          setLoading(false);
+          console.log("falling over")
+          throw new Error(`response status: ${response.status}`);
+        }
+        const responseData = await response.json();
+        console.log(responseData['message']);
+      } catch (error) {
+        setLoading(false);
+        setError(true);
+        console.log('Error sending message: ', error);
+      }
     };
   };
 
