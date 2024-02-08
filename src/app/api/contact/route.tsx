8ac1129e92a92cdde3b0657f2339b3f54b25eb47
Aredu89 <aredu_r89@hotmail.com>
2024-02-08 12:20:26 -0600

    HLD-Resend lib to send emails

--- src/app/api/contact/route.tsx
+++ src/app/api/contact/route.tsx
@@ -1,10 +1,17 @@
 import { NextResponse, NextRequest } from 'next/server';
-const nodemailer = require('nodemailer');
+import { Resend } from 'resend';
+// const nodemailer = require('nodemailer');
+
+export const resend = new Resend(process.env.NEXT_PUBLIC_RESEND_API_KEY!);
+
+// We are using an mail API service called Resend
+// To avoid not being able to send an email when connected to a VPN
 
 export async function POST(request: NextRequest) {
-  const username = process.env.NEXT_PUBLIC_EMAIL_USERNAME;
-  const password = process.env.NEXT_PUBLIC_EMAIL_PASSWORD;
-  const myEmail = process.env.NEXT_PUBLIC_PERSONAL_EMAIL;
+  // const username = process.env.NEXT_PUBLIC_EMAIL_USERNAME;
+  // const password = process.env.NEXT_PUBLIC_EMAIL_PASSWORD;
+  const myEmail = process.env.NEXT_PUBLIC_PERSONAL_EMAIL || '';
+  const workEmail = process.env.NEXT_PUBLIC_HLD_EMAIL || '';
 
   const reqData = await request.json();
   const {
@@ -14,37 +21,62 @@ export async function POST(request: NextRequest) {
     message
   } = reqData;
 
-  const transporter = nodemailer.createTransport({
-    host: "smtp-mail.outlook.com",
-    port: 587,
-    tls: {
-      ciphers: "SSLv3",
-      rejectUnauthorized: false,
-    },
-    auth: {
-      user: username,
-      pass: password
-    }
-  });
-
   try {
-    await transporter.sendMail({
-      from: username,
-      to: myEmail,
-      replyTo: email,
+    const response = await resend.emails.send({
+      from: workEmail,
+      to: [myEmail],
       subject: `HLD - Contact from website: ${email}`,
       html: `
-      <p>Name: ${name} </p>
-      <p>Company: ${company} </p>
-      <p>Email: ${email} </p>
-      <p>Message: ${message} </p>
-      `,
+        <p>Name: ${name} </p>
+        <p>Company: ${company} </p>
+        <p>Email: ${email} </p>
+        <p>Message: ${message} </p>
+      `
     });
-    return NextResponse.json({ message: "Success: email was sent" });
-  } catch (error) {
-      console.log(error)
-      return NextResponse.json({ message: "Error: Could not send message" }, { status: 500 });
-  };
+    console.log('Response::: ', response);
+    if(!response.error) {
+      return NextResponse.json({ message: "Success: email was sent" });
+    } else {
+      console.log(response.error);
+      return NextResponse.json({ message: `Error: ${response.error.message}` }, { status: 500 });
+    }
+  } catch(err) {
+    console.log(err);
+    return NextResponse.json({ message: "Error: Could not send message" }, { status: 500 });
+  }
+
+  // const transporter = nodemailer.createTransport({
+  //   host: "smtp-mail.outlook.com",
+  //   port: 587,
+  //   secure: false,
+  //   tls: {
+  //     ciphers: "SSLv3",
+  //     rejectUnauthorized: false,
+  //   },
+  //   auth: {
+  //     user: username,
+  //     pass: password
+  //   }
+  // });
+
+  // try {
+  //   await transporter.sendMail({
+  //     from: username,
+  //     to: myEmail,
+  //     replyTo: email,
+  //     subject: `HLD - Contact from website: ${email}`,
+  //     html: `
+  //     <p>Name: ${name} </p>
+  //     <p>Company: ${company} </p>
+  //     <p>Email: ${email} </p>
+  //     <p>Message: ${message} </p>
+  //     `,
+  //   });
+  //   return NextResponse.json({ message: "Success: email was sent" });
+  // } catch (error) {
+  //     console.log(error)
+  //     return NextResponse.json({ message: "Error: Could not send message" }, { status: 500 });
+  // };
 };
 
 export const config = {
