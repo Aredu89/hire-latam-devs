049ec52e3c14b100696f40886ff6416554a3a6ea
Aredu89 <aredu_r89@hotmail.com>
2024-02-12 12:11:52 -0600

    HLD-responsive menu

--- src/app/components/collapseMenu/collapseMenu.component.tsx
+++ src/app/components/collapseMenu/collapseMenu.component.tsx
@@ -1,9 +1,31 @@
 import { menuItem } from "../menuItem/menu-item.types";
+import { v4 as uuidv4 } from 'uuid';
+import Link from 'next/link';
 import styles from './collapseMenu.module.css';
+import { useCollapsedMenu } from "@/app/context/collapsedMenu.context";
+import { useContactForm } from "@/app/context/contactForm.context";
 
 const CollapseMenu = ({ items, open }: { items: menuItem[], open: boolean }) => {
+    const { toggleOpen } = useCollapsedMenu();
+    const { toggleFormContactOpen } = useContactForm();
     return (
-        <div className={`${styles.container} ${open ? styles.open : ''}`}>Collapse Menu</div>
+        <div className={`${styles.container} ${open ? styles.open : ''}`}>
+            {items.map(item => (
+                <div key={uuidv4()}>
+                    <Link className={styles.menuItem} href={item.slug} onClick={toggleOpen}>{item.title}</Link>
+                </div>
+            ))}
+            <div
+                className={`${styles.menuItem} ${styles.contact}`}
+                onClick={(e) => {
+                    e.preventDefault();
+                    toggleOpen();
+                    toggleFormContactOpen();
+                }}
+            >
+                Contact Us!
+            </div>
+        </div>
     );
 };
 
