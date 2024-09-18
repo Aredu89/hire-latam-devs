58a92db4fdaec5006eab67d40234008fd152d60a
Aredu89 <aredu_r89@hotmail.com>
2024-09-18 15:11:49 -0500

    HLD-Update header text

--- src/app/layouts/mainSection/mainSection.layout.tsx
+++ src/app/layouts/mainSection/mainSection.layout.tsx
@@ -5,7 +5,7 @@ import styles from './mainSection.module.css';
 
 const MainSection = () => (
   <section className={styles.sectionContainer}>
-    <h4>Find the top software engineers in Latin America</h4>
+    <h4>Find top-tier Software Engineers in Latin America</h4>
     <h2>
       <p>Save money on development costs:</p>
       <p>Hiring staff from LATAM can save you up to 35% in salaries.</p>
