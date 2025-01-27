e26e8b9a5b217f4b2e819de8c173ae77ddbd0c11
Aredu89 <aredu_r89@hotmail.com>
2025-01-27 10:57:52 -0600

    HLD-Improve scroll behavior and update main section text

--- src/app/layouts/mainSection/mainSection.layout.tsx
+++ src/app/layouts/mainSection/mainSection.layout.tsx
@@ -7,8 +7,7 @@ const MainSection = () => (
   <section className={styles.sectionContainer}>
     <h4>Find top-tier Software Engineers in Latin America</h4>
     <h2>
-      <p>Save money on development costs:</p>
-      <p>Hiring staff from LATAM can save you more than 40% in salaries.</p>
+      <p>Save more than 40% on development cost hiring Software Engineers from LATAM</p>
     </h2>
     <h5>Make your hiring process smoother with our help.</h5>
     <div className={styles.latamMapContainer}>
