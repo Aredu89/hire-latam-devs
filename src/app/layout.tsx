58efb1fe280b553ec58321c69e020496ca151913
Aredu89 <aredu_r89@hotmail.com>
2025-01-29 15:58:12 -0600

    HLD-Update viewport meta tag and url for social media

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,7 +7,8 @@ const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: 'HireLatamDevs',
-  description: 'Hire the best talent in Latin America'
+  description: 'Hire the best talent in Latin America',
+  viewport: 'width=device-width, initial-scale=1'
 }
 
 export default function RootLayout({
